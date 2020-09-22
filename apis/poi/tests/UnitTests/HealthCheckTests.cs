1f0d351e88caaae478ba1ed40158b82dbd916539
David Pazdera <david.pazdera@outlook.com>
2020-09-22 16:25:40 +0200

    Update HealthCheckTests.cs
    Testing our GH Action for filing a bug in AzDO.

--- apis/poi/tests/UnitTests/HealthCheckTests.cs
+++ apis/poi/tests/UnitTests/HealthCheckTests.cs
@@ -10,6 +10,7 @@ namespace UnitTests
         public void HealthCheckTestModel()
         {
             Assert.Equal("POI Service Healthcheck", new Healthcheck().Message);
+            Assert.Equal("Broken", new Healthcheck().Status);
             Assert.Equal("Healthy", new Healthcheck().Status);
 
         }
