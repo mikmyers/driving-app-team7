a0cbe95765e78ed2b1ff2acf2b7daeb686b223d3
David Pazdera <david.pazdera@outlook.com>
2020-09-23 09:25:14 +0200

    Update HealthCheckTests.cs

--- apis/poi/tests/UnitTests/HealthCheckTests.cs
+++ apis/poi/tests/UnitTests/HealthCheckTests.cs
@@ -10,7 +10,6 @@ namespace UnitTests
         public void HealthCheckTestModel()
         {
             Assert.Equal("POI Service Healthcheck", new Healthcheck().Message);
-            Assert.Equal("Broken", new Healthcheck().Status);
             Assert.Equal("Healthy", new Healthcheck().Status);
 
         }
